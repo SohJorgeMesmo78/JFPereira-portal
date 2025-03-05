aea307193bb416940cf695a2f9a5e6d9c0198ea2
SohJorgeMesmo78 <jf78pereira@gmail.com>
2025-03-05 16:38:33 -0300

    deixando habilidades tecnicas automatico

--- src/app/components/habilidades-tecnicas/habilidades-tecnicas.component.ts
+++ src/app/components/habilidades-tecnicas/habilidades-tecnicas.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrl: './habilidades-tecnicas.component.scss'
 })
 export class HabilidadesTecnicasComponent {
+  habilidades = [
+    { nome: 'HTML'},
+    { nome: 'CSS'},
+    { nome: 'Angular'},
+    { nome: '.NET', icone: 'DotNet' },
+    { nome: 'JavaScript'},
+    { nome: 'TypeScript'}
+  ];
 
+  getIcone(habilidade: any): string {
+    // Verifica se a habilidade tem o campo 'icone', senão cria o caminho dinâmico
+    return habilidade.icone 
+      ? `assets/icons/${habilidade.icone}.svg` 
+      : `assets/icons/${habilidade.nome}.svg`;
+  }
 }
