86b9edef8bb67efe5febef69f898f3474251958e
SohJorgeMesmo78 <jf78pereira@gmail.com>
2025-03-18 10:33:08 -0300

    removendo console.logs

--- src/app/contatos/contatos.component.ts
+++ src/app/contatos/contatos.component.ts
@@ -10,6 +10,5 @@ export class ContatosComponent {
     { icone: 'fas fa-envelope', nome: 'Email', valor: 'jf78pereira@gmail.com', link: 'mailto:jf78pereira@gmail.com?subject=Contato%20pelo%20site' },
     { icone: 'fas fa-phone-alt', nome: 'Telefone', valor: '+55 13 99138-3830', link: 'https://w.app/sohjorgemesmo78' },
     { icone: 'fab fa-linkedin', nome: 'LinkedIn', valor: 'Jorge Fernando Pereira', link: 'https://www.linkedin.com/in/sohjorgemesmo78/' },
-    // { icone: 'fab fa-instagram', nome: 'Instagram', valor: '@exemplo', link: 'https://www.instagram.com/exemplo' }
   ];
 }
