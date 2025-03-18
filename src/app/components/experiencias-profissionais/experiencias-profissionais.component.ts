371178b4f0c6c6aaa6dd140007947088a4d50dc1
SohJorgeMesmo78 <jf78pereira@gmail.com>
2025-03-18 15:55:42 -0300

    Adcionando transform nas experiencias

--- src/app/components/experiencias-profissionais/experiencias-profissionais.component.ts
+++ src/app/components/experiencias-profissionais/experiencias-profissionais.component.ts
@@ -9,21 +9,21 @@ import { IconeService } from '../../services/icone.service';
 export class ExperienciasProfissionaisComponent {
   experiencias = [
     {
-      empresa: 'Modal Gestão e Resultados Ltda (Modal GR)',
+      empresa: 'Modal GR',
       cargo: 'Analista de Desenvolvimento Júnior',
       descricao: 'Desenvolvimento de aplicações FrontEnd em Angular e BackEnd em .Net.',
       dataInicio: '2022',
       dataFim: ''
     },
     {
-      empresa: 'Modal Gestão e Resultados Ltda (Modal GR)',
+      empresa: 'Modal GR',
       cargo: 'Estagiário em Analista de Sistemas',
       descricao: 'Desenvolvimento de aplicações em .Net.',
       dataInicio: '2021',
       dataFim: '2021'
     },
     {
-      empresa: 'Companhia de Habitação da Baixada Santista (COHAB ST)',
+      empresa: 'COHAB ST',
       cargo: 'Estagiário em T.I.',
       descricao: 'Atendimento HelpDesk para usuários da empresa, participando das rotinas de suporte técnico e auxiliando na gestão de manutenção preventiva das máquinas em operação.',
       dataInicio: '2018',
