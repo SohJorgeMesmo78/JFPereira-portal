33f0c2992ec2151e0ed4e0b64880cd9e264e2bb0
SohJorgeMesmo78 <jf78pereira@gmail.com>
2025-03-18 13:25:10 -0300

    adicionando whats app

--- src/app/contatos/contatos.component.ts
+++ src/app/contatos/contatos.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class ContatosComponent {
   contatos = [
     { icone: 'fas fa-envelope', nome: 'Email', valor: 'jf78pereira@gmail.com', link: 'mailto:jf78pereira@gmail.com?subject=Contato%20pelo%20site' },
-    { icone: 'fas fa-phone-alt', nome: 'Telefone', valor: '+55 13 99138-3830', link: 'https://wa.me/13991383830' },
+    { icone: 'fab fa-whatsapp', nome: 'WhatsApp', valor: '+55 13 99138-3830', link: 'https://wa.me/13991383830' },
     { icone: 'fab fa-linkedin', nome: 'LinkedIn', valor: 'Jorge Fernando Pereira', link: 'https://www.linkedin.com/in/sohjorgemesmo78/' },
   ];
 }
